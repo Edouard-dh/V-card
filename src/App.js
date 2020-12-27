3a67877ccd30e7990b277050114a2f0878ab46a8
Edouard Dhalluin <ed.dhalluin@gmail.com>
2020-12-27 15:14:02 +0100

    Ajout du responsive pour tablette

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './scss/main.scss';
-import Body_top from'./components/Body_top';
 import Body_left from'./components/Body_left';
 import Body_right from'./components/Body_right';
 
@@ -8,8 +7,7 @@ import Body_right from'./components/Body_right';
 function App() {
   
   return (
-    <div className="App">
-      {/* <Body_top/> */}
+    <div className="App_top">
       <Body_left/>
       <Body_right/>
     </div>
